a0bd344342022a6f79e149a36647b82a93290c82
StellarScript <ej92@protonmail.com>
2024-01-11 07:24:10 -0800

    setup cdk app

---
+++ apps/cdk/vitest.config.ts
@@ -0,0 +1,26 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+   plugins: [
+      nxViteTsPaths(),
+      swc.vite({
+         module: { type: 'es6' },
+      }),
+   ],
+   test: {
+      globals: true,
+      cache: {
+         dir: '../../node_modules/.vitest',
+      },
+      environment: 'jsdom',
+      include: ['apps/cdk/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+      reporters: ['default'],
+      coverage: {
+         reportsDirectory: '../../coverage/apps/cdk',
+         provider: 'v8',
+      },
+   },
+});
