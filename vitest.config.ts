a19097fcf79fb9da904c5fbcaf158656f8245535
StellarScript <ej92@protonmail.com>
2024-01-11 06:53:08 -0800

    client vitest setup

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  plugins: [tsconfigPaths()],
-});
